3cc2d45483c320917ebf01e47278a958323ab05e
dglav <glavich_drew@yahoo.com>
2019-09-25 23:56:06 +0900

    Updates

---
+++ precache-manifest.be97c36f06d4b62552dc758f08dfe9a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0817c9442e08f2d878178418e61178",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b349ae3a54c2e41e2081",
+    "url": "/monsters-rolodex/static/css/main.ec3a0ddb.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "b349ae3a54c2e41e2081",
+    "url": "/monsters-rolodex/static/js/main.23fed5e2.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
